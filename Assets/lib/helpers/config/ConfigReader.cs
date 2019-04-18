a3ced3a7e9956d5b89ef4829cc722b2f0de7405a
Rynco Li <lynzrand@outlook.com>
2019-04-18 09:17:18 +0800

    Add HOCON and starting to create config reader

---
+++ Assets/lib/helpers/config/ConfigReader.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using Hocon;
+
+namespace Sesim.Helpers.Config
+{
+    public class ConfigReader
+    {
+
+        private void read()
+        {
+        }
+    }
+
+}
