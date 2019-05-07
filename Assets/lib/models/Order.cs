b6dafef7367e34caf9486fa97244c1b10f195521
Maplecr <49382902+Maplecr@users.noreply.github.com>
2019-05-07 20:31:03 +0800

    Update Order.cs

--- Assets/lib/models/Order.cs
+++ Assets/lib/models/Order.cs
@@ -7,26 +7,26 @@ namespace Sesim.Models
     public partial class Order
     {
         //订单名字
-        public string name;
+        public string Name;
 
         //工程的完成难度，随着公司名气的提高可以接到更难的订单
-        public int degree_of_difficulty;
+        public int DegreeOfDifficulty;
  
         //工程的报酬，定金以及没有按时完成的违约金
-        public decimal reward;
-        public decimal deposit;
-        public decimal breach_of_contract;
+        public decimal Reward;
+        public decimal Deposit;
+        public decimal BreachOfContract;
 
         //适合的语言可以加快进度
-        public string language;
-        //public string methods;
+        public string Language;
+        //public string Methods;
         //先不设定使用的方法
 
-        public int time;
+        public int Time;
         //完成工程的限定时间
-        public int quantities;
+        public int Quantities;
         //根据工程的等级以及工程的大小安排工程的工作量
-        public int Number_of_employees;
+        public int NumberOfEmployees;
         //限定完成订单的人数
     }
 }
