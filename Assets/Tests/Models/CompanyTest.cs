58b4ec758c8b63ee67b13f773672a959cf00896d
Rynco Li <lynzrand@outlook.com>
2019-05-27 14:46:17 +0800

    Fix Failed Test: CompanyTest#CompanyUpdateTest

--- Assets/Tests/Models/CompanyTest.cs
+++ Assets/Tests/Models/CompanyTest.cs
@@ -39,7 +39,7 @@ namespace Tests.Models
                 completedWork = 0.0,
                 techStack = "csharp"
             });
-            mockCompany.contractFactories.Add(new ContractFactory());
+            mockCompany.contractFactories.Add(new ContractFactory().SetDebugDefault());
         }
 
 
